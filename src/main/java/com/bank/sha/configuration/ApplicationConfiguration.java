54e3f0394ee48b514a4e5627d6b1bd1fe973f07f
hadipsy27 <hadipsy27@gmail.com>
2025-05-13 11:46:02 +0700

    Remove bean AuthenticationProvider because no use in spring jwt

--- src/main/java/com/bank/sha/configuration/ApplicationConfiguration.java
+++ src/main/java/com/bank/sha/configuration/ApplicationConfiguration.java
@@ -5,8 +5,6 @@ import com.bank.sha.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -31,14 +29,6 @@ public class ApplicationConfiguration {
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
     }
 
     @Bean
