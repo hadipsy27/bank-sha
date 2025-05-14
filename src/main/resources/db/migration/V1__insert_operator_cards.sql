01281a54f47cdf9514c0d6f049a41e9de4f802ef
hadipsy27 <hadipsy27@gmail.com>
2025-05-14 15:44:38 +0700

    Add database migration flyway

---
+++ src/main/resources/db/migration/V1__insert_operator_cards.sql
@@ -0,0 +1,6 @@
+-- Insert operator cards
+INSERT INTO operator_cards (name, status, thumbnail, created_at, updated_at) VALUES
+('Telkomsel', 'ACTIVE', 'telkomsel.jpg',  '2025-05-14 10:00:00',  '2025-05-14 10:00:00'),
+('Indosat', 'ACTIVE', 'indosat.jpg',  '2025-05-14 10:00:00',  '2025-05-14 10:00:00'),
+('XL', 'ACTIVE', 'xl.jpg',  '2025-05-14 10:00:00',  '2025-05-14 10:00:00'),
+('MTN', 'ACTIVE', 'mtn.jpg',  '2025-05-14 10:00:00',  '2025-05-14 10:00:00');
